74b0cbe11ed8ff8465dceb708add5789cec499a0
FIFSAK <anuar200572@gmail.com>
2024-02-18 17:12:18 +0600

    lab4

--- Lab4/src/app/product-list/product-list.component.ts
+++ Lab4/src/app/product-list/product-list.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { products } from '../products';
+import {products} from '../products';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = [...products];
+    products = [...products];
 
-  share() {
-    window.alert('The product has been shared!');
-  }
+    share(text: string) {
+        navigator.clipboard.writeText(text).then(function () {
+            console.log('Async: Copying to clipboard was successful!');
+            window.open('https://web.telegram.org/k/', '_blank');
+        }, function (err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+
+    }
 }
 
 
