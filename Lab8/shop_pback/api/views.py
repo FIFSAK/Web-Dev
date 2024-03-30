00d99fe62930c062ebb9b735c5ab7d2aaeafd3ab
FIFSAK <anuar200572@gmail.com>
2024-03-30 11:21:45 +0500

    finish lab8

--- Lab8/shop_pback/api/views.py
+++ Lab8/shop_pback/api/views.py
@@ -1,14 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 from django.core import serializers
 
 
-def get_products(request):
+def get_products(request, id=None):
     if request.method == 'GET':
         products = Product.objects.all()
+        if id:
+            products = products.filter(id=id)
         data = serializers.serialize("json", products)
 
-        return HttpResponse(data, content_type='application/json')
+        return HttpResponse(data, content_type='application/json', status=200)
     else:
-        return render(request, "Error Pages/405.html", status=405)
+        return HttpResponse(status=405, reason='Method Not Allowed')
+
+
+def get_categories(request, id=None):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        if id:
+            categories = categories.filter(id=id)
+        data = serializers.serialize("json", categories)
+
+        return HttpResponse(data, content_type='application/json', status=200)
+    else:
+        return HttpResponse(status=405, reason='Method Not Allowed')
+
+def get_categories_products(request, id):
+    if request.method == 'GET':
+        products = Product.objects.filter(category_id=id)
+        data = serializers.serialize("json", products)
+
+        return HttpResponse(data, content_type='application/json', status=200)
+    else:
+        return HttpResponse(status=405, reason='Method Not Allowed')
--- Lab8/shop_pback/shop_pback/urls.py
+++ Lab8/shop_pback/shop_pback/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
-from api.views import get_products
+from api.views import get_products, get_categories, get_categories_products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products', get_products),
+    path('api/products/<int:id>', get_products),
+    path('api/categories', get_categories),
+    path('api/categories/<int:id>', get_categories),
+    path('api/categories/<int:id>/products', get_categories_products),
 ]
+# /api/products - List of all Products
+# /api/products/<int:id>/ - Get one Product
+# /api/categories/ - List of all Categories
+# /api/categories/<int:id>/ - Get one Category
+# /api/categories/<int:id>/products/ - List of Products by Category
