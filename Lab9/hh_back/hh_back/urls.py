4508e014bfa38a8c401162f596837072e2092f63
FIFSAK <anuar200572@gmail.com>
2024-04-04 15:26:38 +0500

    start lab9

---
+++ Lab9/hh_back/hh_back/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for hh_back project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+]
+# /api/companies - List of all Companies
+# /api/companies/<int:id>/ - Get one Company
+# /api/companies/<int:id>/vacancies/ - List of Vacancies by Company
+# /api/vacancies/ - List of all Vacancies
+# /api/vacancies/<int:id>/ - Get one Vacancy
+# /api/vacancies/top_ten/ - List of top 10 vacancies sorted by decreasing salary
